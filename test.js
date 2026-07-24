c5ebdbb2456761a252a07b20fed56ee1e76e958d
hutaochan99 <ahmadzhofir1808@gmail.com>
2026-07-24 08:53:35 +0700

    emote support revisi

--- test.js
+++ test.js
@@ -1,5 +1,19 @@
-console.log("Start");
+// test_emoji.js
+const { createCanvas, GlobalFonts } = require("@napi-rs/canvas");
+const fs = require("fs");
 
-setInterval(() => {
-    console.log("Masih hidup...");
-}, 5000);
+console.log("Fonts terdaftar sebelum:", GlobalFonts.families.length);
+const ok1 = GlobalFonts.registerFromPath("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", "MemeFont");
+const ok2 = GlobalFonts.registerFromPath("/usr/share/fonts/truetype/noto/NotoColorEmoji.ttf", "MemeEmoji");
+console.log("Register DejaVu:", ok1, " Register Emoji:", ok2);
+
+const canvas = createCanvas(300, 150);
+const ctx = canvas.getContext("2d");
+ctx.font = 'bold 46px "MemeFont", "MemeEmoji"';
+ctx.textBaseline = "middle";
+ctx.fillStyle = "black";
+ctx.fillText("test😂", 10, 75);
+
+const buf = canvas.toBuffer("image/png");
+fs.writeFileSync("test_emoji.png", buf);
+console.log("Selesai, cek test_emoji.png. Ukuran:", buf.length, "bytes");
